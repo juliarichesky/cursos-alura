40bd812dfd40d894875948eee091037c438e0153
julia :) <juliavaleriogs@gmail.com>
2025-09-23 15:41:17 -0300

    lógica de programação com javascript

---
+++ iniciante-programacao/exercicios/ex1.js
@@ -0,0 +1,17 @@
+alert('Boas vindos ao nosso site!');
+let nome = 'Lua';
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert('Erro! Preencha todos os campos!');
+
+let mensagemDeErro = ('Erro! Preencha todos os campos!');
+alert (mensagemDeErro);
+
+nome = prompt('Qual o seu nome?');
+idade = prompt('Qual a sua idade?');
+
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!');
+}
---
+++ iniciante-programacao/logica-js-projeto_inicial/app.js
@@ -0,0 +1,11 @@
+alert('Boas vindas ao jogo do número secreto!');
+let numeroSecreto = 5;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um número entre 1 a 10');
+
+// se o chute for igual a numero secreto
+if (chute == numeroSecreto) {
+    alert('Isso aí! Você descobriu o número secreto!');
+} else {
+    alert('Você errou! :(');
+}
