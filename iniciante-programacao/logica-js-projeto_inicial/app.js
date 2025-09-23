fe57558711c9c4faf6bce61fecb9061e9e1e51e7
julia :) <juliavaleriogs@gmail.com>
2025-09-23 17:02:34 -0300

    template string

--- iniciante-programacao/logica-js-projeto_inicial/app.js
+++ iniciante-programacao/logica-js-projeto_inicial/app.js
@@ -5,7 +5,8 @@ let chute = prompt('Escolha um número entre 1 a 10');
 
 // se o chute for igual a numero secreto
 if (chute == numeroSecreto) {
-    alert('Isso aí! Você descobriu o número secreto!');
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}!`);
+                                                    //template strings
 } else {
     alert('Você errou! :(');
 }
